1597ef4e8e307f12d0c26bb9937fb33a4e6da354
null <60427892+null8626@users.noreply.github.com>
2022-10-23 18:36:15 +0700

    refactor(__init__): amogus

--- vtuberwiki/__init__.py
+++ vtuberwiki/__init__.py
@@ -1,8 +1,26 @@
-"""VtuberwikiPy
-====================================
+"""
+VtuberwikiPy
+a Python wrapper for VtuberWiki Fandom API
+The MIT License (MIT)
+Copyright (c) 2022 daffpy (http://github.com/daffpy/vtuberwiki-py)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
-from vtuberwiki.aiovwiki import AioVwiki
-from vtuberwiki.vwiki import Vwiki
+from .aiovwiki import AioVwiki
+from .vwiki import Vwiki
 
-__all__ = ["AioVwiki,Vwiki"]
+__all__ = ("AioVwiki", "Vwiki")
